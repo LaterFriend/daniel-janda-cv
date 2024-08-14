c4850a5a7ede8543aee43c6e7c3735aa01a96ea3
Daniel Janda <danieljanda75@seznam.cz>
2024-08-14 23:42:58 +0200

    Day2

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,15 +1,18 @@
-import ProjectCard from "../../common/ProjectCard"
-import styles from "./Projects.module.css"
+import ProjectCard from "../../common/ProjectCard";
+import styles from "./Projects.module.css";
 
 function Projects() {
-  return (
-    <section id="projects" className={styles.container}>
-        <h1 className="sectionTitle">Projects</h1>
-        <div className={styles.projectsContainer}>
-            <ProjectCard />
-        </div>
-    </section>
-  )
+    return (
+        <section
+            id="projects"
+            className={styles.container}
+        >
+            <h1 className="sectionTitle">Projects</h1>
+            <div className={styles.projectsContainer}>
+                <ProjectCard />
+            </div>
+        </section>
+    );
 }
 
-export default Projects
+export default Projects;
