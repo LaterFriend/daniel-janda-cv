c8ff3d06c4750cc96c8d34436adc51ee2d8020d5
Daniel Janda <danieljanda75@seznam.cz>
2024-08-15 16:30:49 +0200

    Finished

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import styles from "./HeroStyles.module.css";
-import heroImg from "../../assets/hero-img.png";
+import heroImg from "../../assets/profile_cropped.png";
 import sun from "../../assets/sun.svg";
 import moon from "../../assets/moon.svg";
 import linkedinLight from "../../assets/linkedin-light.svg";
@@ -10,7 +10,7 @@ import CV from "../../assets/Životopis - Janda Daniel.pdf";
 import { useTheme } from "../../common/ThemeContext";
 
 function Hero() {
-    const {theme, toggleTheme} = useTheme();
+    const { theme, toggleTheme } = useTheme();
     const themeIcon = theme === "light" ? sun : moon;
     const linkedinIcon = theme === "light" ? linkedinLight : linkedinDark;
     const githubIcon = theme === "light" ? githubLight : githubDark;
